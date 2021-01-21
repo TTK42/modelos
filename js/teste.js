d73a7b0381b77e9f4333f4eacbe5d9a309957779
Tooru <tooru@agenciaalper.com.br>
2021-01-21 12:33:22 -0200

    corrigido e adicionado modelos

--- js/teste.js
+++
@@ -1,7 +0,0 @@
-{/* <script type="text/javascript"></script>
-var resultado = document.querySelectorAll('.woocommerce-products-wizard-control-inner');
-for(i=0;i<resultado.length;i++){ if(resultado[i].innerHTML.replace(/\s/g, '') == 'Toresults'){ resultado[i].innerHTML = "Resultado"; } }
-
-var estoque = document.querySelector('.in-stock');
-estoque.textContent = "Em estoque"; */}
-
